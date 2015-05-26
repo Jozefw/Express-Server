f34abe8f08be8bc6786c65bffc10d66c9b8b7c06
Jozef <jozefw@sbcglobal.net>
2015-05-26 12:41:12 -0700

    dynamic website with node/express

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var hbs = require('hbs');
+// requires the blog.js page and calls it blogEngine
+var blogEngine = require('./blog');
+
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+app.use(express.bodyParser());
+
+app.get('/', function(request, response){
+	response.render('index', {title: "My Blog", entries:blogEngine.getBlogEntries()});
+
+});
+
+app.get('/about',function(request,response){
+	response.render('about', {title: "About Me"});
+});
+
+app.get('/article/:id', function(request,response){
+	var entry = blogEngine.getBlogEntry(request.params.id);
+
+	response.render('article', {title:entry.title, blog:entry});
+});
+
+app.listen(3000);
